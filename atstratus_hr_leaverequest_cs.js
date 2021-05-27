fc07b1dc2e814dd754910b2dd4bd9fd8034bb504
Raphael Baligod <raphael.baligod@yahoo.com>
2021-05-27 19:23:39 +0800

    Added initial module changes for the Date Verification, as well as date calculation.

--- atstratus_hr_leaverequest_cs.js
+++ atstratus_hr_leaverequest_cs.js
@@ -3,26 +3,12 @@
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
- define(['N/record', 'N/search'],
+ define(['N/record', 'N/search', 'N/ui/dialog'],
  /**
   * @param {record} record
   * @param {search} search
   */
- function(record, search) {
-     
-     /**
-      * Function to be executed after page is initialized.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-      *
-      * @since 2015.2
-      */
-     function pageInit(scriptContext) {
-         
-     }
- 
+ function(record, search, dialog) {
      /**
       * Function to be executed when field is changed.
       *
@@ -36,112 +22,27 @@
       * @since 2015.2
       */
      function fieldChanged(scriptContext) {
-         var currRec = scriptContext.newRecord;
+        var currRec = scriptContext.currentRecord;
  
-         log.debug('fieldChanged executed', scriptContext.fieldId);
-     }
- 
-     /**
-      * Function to be executed when field is slaved.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      * @param {string} scriptContext.fieldId - Field name
-      *
-      * @since 2015.2
-      */
-     function postSourcing(scriptContext) {
- 
-     }
- 
-     /**
-      * Function to be executed after sublist is inserted, removed, or edited.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      *
-      * @since 2015.2
-      */
-     function sublistChanged(scriptContext) {
- 
-     }
- 
-     /**
-      * Function to be executed after line is selected.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      *
-      * @since 2015.2
-      */
-     function lineInit(scriptContext) {
- 
-     }
- 
-     /**
-      * Validation function to be executed when field is changed.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      * @param {string} scriptContext.fieldId - Field name
-      * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-      * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-      *
-      * @returns {boolean} Return true if field is valid
-      *
-      * @since 2015.2
-      */
-     function validateField(scriptContext) {
- 
-     }
- 
-     /**
-      * Validation function to be executed when sublist line is committed.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      *
-      * @returns {boolean} Return true if sublist line is valid
-      *
-      * @since 2015.2
-      */
-     function validateLine(scriptContext) {
- 
-     }
- 
-     /**
-      * Validation function to be executed when sublist line is inserted.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      *
-      * @returns {boolean} Return true if sublist line is valid
-      *
-      * @since 2015.2
-      */
-     function validateInsert(scriptContext) {
- 
-     }
- 
-     /**
-      * Validation function to be executed when record is deleted.
-      *
-      * @param {Object} scriptContext
-      * @param {Record} scriptContext.currentRecord - Current form record
-      * @param {string} scriptContext.sublistId - Sublist name
-      *
-      * @returns {boolean} Return true if sublist line is valid
-      *
-      * @since 2015.2
-      */
-     function validateDelete(scriptContext) {
- 
+        log.debug('fieldChanged executed', scriptContext.fieldId);
+        try{
+            //If Start or End date is modified, calculate for the days difference.
+           if(scriptContext.fieldId == 'custrecord_atstratus_hr_lr_start_date' || scriptContext.fieldId == 'custrecord_atstratus_hr_lr_end_date'){
+               if(currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_start_date'}) && currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_end_date'})){
+                    var startDate = currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_start_date'});
+                    var endDate = currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_end_date'});
+
+                    //86,400,000 = 1-day.
+                    var dayDifference = (endDate - startDate) / 86400000;
+                    //Set the calculated difference
+                    currRec.setValue({ fieldId: 'custrecord_atstratus_hr_lr_duration', value: dayDifference});
+               }
+           }
+        }
+        catch(e){
+            log.error('Error occured on fieldChanged', e);
+            return false;
+        }
      }
  
      /**
@@ -154,19 +55,55 @@
       * @since 2015.2
       */
      function saveRecord(scriptContext) {
+        var currRec = scriptContext.currentRecord;
  
+        log.debug('fieldChanged executed', scriptContext.fieldId);
+        try{
+            var startDate = currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_start_date'});
+            var endDate = currRec.getValue({ fieldId: 'custrecord_atstratus_hr_lr_end_date'});
+
+            log.debug('Start date', startDate);
+            log.debug('End date', endDate);
+
+            var yesterdayDate = new Date();
+            yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+            
+            //86,400,000 = 1-day.
+            var dayDifference = (endDate - startDate) / 86400000;
+
+            //Validators. Check for Date overlaps. There should be none before saving the record.
+            if(yesterdayDate > startDate){
+                dialog.alert({
+                    title: 'Alert',
+                    message: 'Start Date cannot be before today.'
+                });
+                return false;
+            }
+            if(yesterdayDate > endDate){
+                dialog.alert({
+                    title: 'Alert',
+                    message: 'End Date cannot be before today.'
+                });
+                return false;
+            }
+            if(dayDifference <= 0){
+                dialog.alert({
+                    title: 'Alert',
+                    message: 'Start Date cannot be after or on the End date.'
+                });
+                return false;
+            }
+        }
+        catch(e){
+            log.error('Error occured on fieldChanged', e);
+            return false;
+        }
+
+        return true;
      }
  
      return {
-         pageInit: pageInit,
          fieldChanged: fieldChanged,
-         postSourcing: postSourcing,
-         sublistChanged: sublistChanged,
-         lineInit: lineInit,
-         validateField: validateField,
-         validateLine: validateLine,
-         validateInsert: validateInsert,
-         validateDelete: validateDelete,
          saveRecord: saveRecord
      };
      
